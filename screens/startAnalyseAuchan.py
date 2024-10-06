e597ae1d406db463f8869e4c531148cecffe8bac
magnie0 <magdalenanieszporska@gmail.com>
2024-10-06 11:04:42 +0200

    Refactor and corrected ui when changing file name

--- screens/startAnalyseAuchan.py
+++ screens/startAnalyseAuchan.py
@@ -13,29 +13,38 @@ class AuchanAnalyseScreen(Screen):
         
         # Create BoxLayout and add buttons
         self.layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
         self.file_name = TextInput(text='2024-09-13.jpg', multiline=False)
         self.layout.add_widget(self.file_name)
+        self.file_name.bind(text = self.change_input_file_listener)
+
         LoadLayout = BoxLayout(orientation='horizontal', spacing=10)
         self.LoadFileButton = Button(text="LoadFile", on_press=self.load_file)
         self.LoadFileLabel = Label(text='Not loaded')
         LoadLayout.add_widget(self.LoadFileButton)
         LoadLayout.add_widget(self.LoadFileLabel)
         self.layout.add_widget(LoadLayout)
+
         self.ReloadDatabaseButton = Button(text="ReloadDatabase", on_press=self.check_database)
         self.layout.add_widget(self.ReloadDatabaseButton)
-        self.SaveResult = Button(text="SaveResult", on_press=self.saveResult)
         self.ReloadDatabaseButton.disabled = True
-        self.SaveResult.disabled = True
+
+        self.SaveResult = Button(text="SaveResult", on_press=self.saveResult)
         self.layout.add_widget(self.SaveResult)
+        self.SaveResult.disabled = True
+        
         self.add_widget(self.layout)
 
         self.name_file = 'auchan.json' #TODO change operations regarding database
 
+    def change_input_file_listener(self, instane, text):
+        self.LoadFileLabel.text = 'Not loaded'
+        self.ReloadDatabaseButton.disabled = True
+        self.SaveResult.disabled = True
 
     def load_file(self, instance):
         input_str = ImagetoText(self.file_name.text)
         date = '2024-09-13'
-    
         self.products = Receipt_To_Product(input_str,date)#without set categories
         self.ReloadDatabaseButton.disabled = False
         self.LoadFileLabel.text = 'Loaded'
@@ -44,8 +53,6 @@ class AuchanAnalyseScreen(Screen):
     def load_database(self):
         with open('./database/'+self.name_file, 'r') as file:
             self.database = json.load(file)
-        print(self.database)
-        print('========================')
         self.flatten_dict()
 
     def flatten_dict(self):
@@ -56,13 +63,20 @@ class AuchanAnalyseScreen(Screen):
                     for element in v2:
                         triplet = (cat0, cat1, cat2)
                         self.flatten_database[element] = triplet
-        print(self.flatten_database)
 
     def part_identifies_product(self, name):
         for part in name.split():
             if part in self.flatten_database:
                 return True 
         return False
+    
+    def get_triple(self, name):
+        if name in self.flatten_database:
+            return self.flatten_database[name]
+        for part in name.split():
+            if part in self.flatten_database:
+                return self.flatten_database[part]
+        
 
     def check_database(self, instance):
         self.load_database()
@@ -79,8 +93,15 @@ class AuchanAnalyseScreen(Screen):
             self.manager.current = 'LoadCategoriesScreen'
         else:
             self.SaveResult.disabled = False
+            for p in self.products:
+                name = p.Get_Name()
+                cat0, cat1, cat2 = self.get_triple(name)
+                p.Set_Category(cat0, cat1, cat2)
+                print(p.To_Array())
+
 
 
 
     def saveResult(self, instance):
         print("Here will be saving")
+        #TODO Here count the discount for products
